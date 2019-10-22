ae755a7e45fdcd3e5cb6cd0de88c94cc0f947fca
Grant Bartone <gbartone@gmail.com>
2019-10-22 09:45:44 -0700

    Initial Github Pages commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactGA from 'react-ga';
+import './App.css';
+
+function App() {
+  initializeReactGA();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Coming Soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+function initializeReactGA() {
+  ReactGA.initialize('G-RJDR3ET5KK');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+export default App;
